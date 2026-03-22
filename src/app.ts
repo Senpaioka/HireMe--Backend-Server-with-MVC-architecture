a84ee2a20e11c2aa4bf582dde6755f9e18910c8b
senpaioka@linux <abdulmannan7589@gmail.com>
2026-03-23 00:01:00 +0600

    app.ts updated

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+
+// Create an Express application
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/v1', routes);
+
+// initial route
+app.get('/', (req, res) => {
+    res.send('Welcome to the HireMe API server!');
+})
+
+// invalid route handler
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: 'Route not found'
+    })
+})
+
+
+export default app;
